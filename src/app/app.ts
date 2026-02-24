e3518b4b584a1aabe49c449d0747acf58ea918b5
JuanMartin19 <2023371152@uteq.edu.mx>
2026-02-23 21:46:38 -0600

    Primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeImportsModule } from './prime-imports';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet , PrimeImportsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('RP');
 }
