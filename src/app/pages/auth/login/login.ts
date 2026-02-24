b5ca3c1687ab428673999259c35fce36c8f8b43d
JuanMartin19 <2023371152@uteq.edu.mx>
2026-02-24 11:20:26 -0600

    Practica 3

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { PrimeImportsModule } from "../../../prime-imports";
 
 @Component({
@@ -10,5 +10,25 @@ import { PrimeImportsModule } from "../../../prime-imports";
   styleUrl: './login.css',
 })
 export class Login {
+  email = signal('');
+  password = signal('');
 
+  private readonly USER = {
+    email: 'admin@gmail.com',
+    password: 'Admin123'
+  };
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (
+      this.email() === this.USER.email &&
+      this.password() === this.USER.password
+    ) {
+      alert('Login correcto');
+      this.router.navigate(['/landing']);
+    } else {
+      alert('Tu correo o contraseñ esta incorrecta');
+    }
+  }
 }
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PrimeImportsModule } from "../../../prime-imports";
 
@@ -11,4 +11,79 @@ import { PrimeImportsModule } from "../../../prime-imports";
 })
 export class Register {
 
+  usuario = signal('');
+  email = signal('');
+  password = signal('');
+  confirmPassword = signal('');
+  nombre = signal('');
+  direccion = signal('');
+  telefono = signal('');
+  nacimiento = signal('');
+
+  mensaje = signal('');
+  tipoMensaje = signal<'success' | 'error' | ''>('');
+
+  validarPassword(pass: string): boolean {
+    const regex = /^(?=.*[!@#$%^&*(),.?":{}|<>]).{10,}$/;
+    return regex.test(pass);
+  }
+
+  esMayorEdad(fecha: string): boolean {
+    const nacimiento = new Date(fecha);
+    const hoy = new Date();
+
+    let edad = hoy.getFullYear() - nacimiento.getFullYear();
+    const m = hoy.getMonth() - nacimiento.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < nacimiento.getDate())) {
+      edad--;
+    }
+
+    return edad >= 18;
+  }
+
+  registrar() {
+
+    if (
+      !this.usuario() ||
+      !this.email() ||
+      !this.password() ||
+      !this.confirmPassword() ||
+      !this.nombre() ||
+      !this.direccion() ||
+      !this.telefono() ||
+      !this.nacimiento()
+    ) {
+      this.tipoMensaje.set('error');
+      this.mensaje.set('Todos los campos son obligatorios');
+      return;
+    }
+
+    if (!this.validarPassword(this.password())) {
+      this.tipoMensaje.set('error');
+      this.mensaje.set('La contraseña debe tener mínimo 10 caracteres y un símbolo especial: !@#$%^&*(),.?":{}|<>');
+      return;
+    }
+
+    if (this.password() !== this.confirmPassword()) {
+      this.tipoMensaje.set('error');
+      this.mensaje.set('Las contraseñas no coinciden');
+      return;
+    }
+
+    if (!/^[0-9]+$/.test(this.telefono())) {
+      this.tipoMensaje.set('error');
+      this.mensaje.set('El teléfono solo debe contener números');
+      return;
+    }
+
+    if (!this.esMayorEdad(this.nacimiento())) {
+      this.tipoMensaje.set('error');
+      this.mensaje.set('Debes ser mayor de edad para registrarte');
+      return;
+    }
+
+    this.tipoMensaje.set('success');
+    this.mensaje.set('Registro exitoso 🎉');
+  }
 }
